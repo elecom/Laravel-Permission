4764658be0b61a4abe2c400a213f9b4dcffcca12
Dixon Acosta <dacostaelecom@gmail.com>
2021-07-21 15:44:43 -0430

    Se quitaron botones del tablero

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,19 +8,6 @@
 
 @section('content')
     
-    {{-- Minimal --}}
-    <x-adminlte-modal id="modalMin" title="Minimal"/>
-    {{-- Example button to open modal --}}
-    <x-adminlte-button label="Open Modal" data-toggle="modal" data-target="#modalMin"/>
-
-    {{-- Themed --}}
-    <x-adminlte-modal id="modalPurple" title="Theme Purple" theme="purple"
-    icon="fas fa-bolt" size='lg'>
-    This is a purple theme modal without animations.
-    </x-adminlte-modal>
-    {{-- Example button to open modal --}}
-    <x-adminlte-button label="Open Modal" data-toggle="modal" data-target="#modalPurple" class="bg-purple"/>
-
 @stop
 
 @section('css')
